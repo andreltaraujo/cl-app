6070bc4bba120ea423323ac3a67310253b4d3607
André Araujo <andreguia@hotmail.com>
2023-09-13 19:08:20 -0300

    - Devise Install - Configuration - Generate models - Generate views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :educs
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
